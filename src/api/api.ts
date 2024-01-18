70440d03ff0f4bc8abe96716f0887f66d42f5f5f
Mizanur Rahman <nickty.86@gmail.com>
2024-01-18 09:16:34 +0600

    more wordes 19425 line

--- src/api/api.ts
+++ src/api/api.ts
@@ -7,25 +7,19 @@ const apiKey = 'AIzaSyDy0DKnuaWIDGLIct8lCnKyWYZEcIMUZl8';
 const BASE_URL = 'http://api.alquran.cloud/v1'; // Replace with your API endpoint
 
 const fetchWordDetails = async (word: string) => {
-  // console.log("workss api", word)
+ 
   try {
     const response = await axios.get(`${BASE_URL}/search/${word}/all/ar`);  
-    // console.log("check res", response)
+    
     if (response.data && response.data.data && response.data.data.matches) {
       const matches = response.data.data.matches;
 
-      // console.log("check matches", matches[0])
-
-      // Extract words from matches (assuming 'matches' contains the words)
       const words: string[] = matches.map((match: any) => {
-        // Assuming each 'match' object contains the word
-        // console.log("first language", match.edition.name)
-        return match.edition.name || ''; // Replace with the field containing the word
+     
+        return match.text || ''; 
       });
 
-      // console.log("check word", words)
-
-      // Get the first five words
+    
       const firstFiveWords = words.slice(0, 5);
 
       return firstFiveWords;
--- src/screen/WordDetailsScreen.jsx
+++ src/screen/WordDetailsScreen.jsx
@@ -3,12 +3,14 @@ import {View, Text, ScrollView, StyleSheet} from 'react-native';
 import {RouteProp} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {fetchWordDetails} from '../api/api';
+import Stemmer from 'arabic-stemmer';
 
 const WordDetailsScreen = ({route}) => {
   const {word} = route.params;
   const [details, setDetails] = useState(''); // Update 'any' with expected API response type
 
-  console.log('check route', details);
+  // console.log('check route', details);
+  const [highlightedWord, setHighlightedWord] = useState('');
 
   useEffect(() => {
     const fetchDetails = async () => {
@@ -24,16 +26,40 @@ const WordDetailsScreen = ({route}) => {
     };
 
     fetchDetails();
+
+    const timeoutId = setTimeout(() => {
+      setHighlightedWord(word);
+    }, 4000);
+
+    // Clear the timeout when the component unmounts
+    return () => clearTimeout(timeoutId);
   }, [word]);
 
+  const highlightText = text => {
+    if (highlightedWord) {
+      const stemmedWord = Stemmer.stem(highlightedWord); // Stem the word
+      console.log('stemmed word', stemmedWord);
+      const regex = new RegExp(stemmedWord, 'ugi'); // u flag for Unicode
+      return text.split(regex).map((part, index) => (
+        <Text
+          key={index}
+          style={index % 2 === 1 ? styles.highlightedText : styles.itemText}>
+          {part}
+        </Text>
+      ));
+    }
+
+    return <Text style={styles.itemText}>{text}</Text>;
+  };
+
   return (
-    <View>
-      <Text>Details for {word}</Text>
-      {details.length > 0 && (
+    <View style={styles.container}>
+      <Text style={styles.header}>Details for {word}</Text>
+      {details && details.length > 0 && (
         <ScrollView style={styles.detailsContainer}>
           {details.map((item, index) => (
             <View key={index} style={styles.itemContainer}>
-              <Text style={styles.itemText}>{item}</Text>
+              {highlightText(item)}
               {/* You can display additional details here for each item */}
             </View>
           ))}
@@ -50,22 +76,28 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   header: {
-    fontSize: 24,
+    fontSize: 30,
     fontWeight: 'bold',
-    marginBottom: 20,
+    marginBottom: 10,
   },
   detailsContainer: {
     flex: 1,
+    marginBottom: 20,
   },
   itemContainer: {
-    marginBottom: 15,
-    padding: 10,
-    borderWidth: 1,
+    borderBottomWidth: 1,
     borderColor: '#ccc',
-    borderRadius: 8,
+    paddingVertical: 10,
   },
   itemText: {
-    fontSize: 16,
+    fontSize: 25,
+    color: '#333',
+  },
+  highlightedText: {
+    fontSize: 25,
+    color: '#333',
+    textDecorationLine: 'underline',
+    backgroundColor: 'yellow', // Adjust the background color as needed
   },
 });
 
